e53f97ea7e88222757fbf8850ed1fa015525987b
lenny <lenny.dennis@zeraki.co.ke>
2024-07-03 13:21:54 +0300

    Use the environment variables as opposed to hardcorded values

--- src/main/java/com/lenny/ncba/country/config/CountryConfiguration.java
+++ src/main/java/com/lenny/ncba/country/config/CountryConfiguration.java
@@ -1,6 +1,7 @@
 package com.lenny.ncba.country.config;
 
 import com.lenny.ncba.country.client.CountryClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -8,17 +9,24 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 @Configuration
 public class CountryConfiguration {
 
+    @Value("${country.info.service.uri}")
+    private String serviceUri;
+
+    @Value("${marshaller.context.path}")
+    private String contextPath;
+
+
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setContextPath("com.lenny.ncba.wsdl");
+        marshaller.setContextPath(contextPath);
         return marshaller;
     }
 
     @Bean
     public CountryClient countryClient(Jaxb2Marshaller marshaller) {
         CountryClient client = new CountryClient();
-        client.setDefaultUri("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso");
+        client.setDefaultUri(serviceUri);
         client.setMarshaller(marshaller);
         client.setUnmarshaller(marshaller);
         return client;
